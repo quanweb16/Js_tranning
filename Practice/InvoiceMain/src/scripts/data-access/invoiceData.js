5c35d58596ff19bfb0d6123c50c208a8f65e0d95
minhquan2002 <minhquan663388@gmail.com>
2024-10-02 10:41:52 +0700

    Refactor: Update DataAccess class name to InvoiceData

---
+++ Practice/InvoiceMain/src/scripts/data-access/invoiceData.js
@@ -0,0 +1,20 @@
+class InvoiceData {
+    constructor() {}
+
+    getInvoices() {
+        const invoices = localStorage.getItem('invoices');
+        return invoices ? JSON.parse(invoices) : [];
+    }
+
+    addInvoice(invoice) {
+        const invoices = this.getInvoices();
+        invoices.push(invoice);
+        this.saveInvoices(invoices);  
+    }
+
+    saveInvoices(invoices) {
+        localStorage.setItem('invoices', JSON.stringify(invoices));
+    }
+}
+
+export default InvoiceData;
