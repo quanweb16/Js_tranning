a7b74b7c1c90a8d9e61fbc6f1503c387fb4f13d6
minhquan2002 <minhquan663388@gmail.com>
2024-10-02 16:38:24 +0700

    Create Invoice class

---
+++ Practice/InvoiceMain/src/scripts/data/invoice.js
@@ -0,0 +1,14 @@
+class Invoice{
+  constructor(id,firstName,lastName,email,city,region,date,status = "Pending"){
+    this.id =id;
+    this.firstName =firstName;
+    this.lastName =lastName;
+    this.email =email;
+    this.city = city;
+    this.region= region;
+    this.date=date;
+    this.status= status ;
+  }
+}
+
+export default Invoice;
