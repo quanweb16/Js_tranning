995705afe82958e42bc98407fb97688efe6c38ab
minhquan2002 <minhquan663388@gmail.com>
2024-10-02 10:38:01 +0700

    Change class name from DataAccess to InvoiceData in App

--- Practice/InvoiceMain/src/scripts/app.js
+++ Practice/InvoiceMain/src/scripts/app.js
@@ -1,18 +1,18 @@
-import InvoiceService from "./business-layer/invoiceService.js";
-import DataAccess from "./data-access-layer/invoiceData.js";
+import InvoiceBusiness from "./business/invoice/invoice.js";
+import InvoiceData from "./data-access/invoiceData.js";
 import InvoicePresentation from "./presentation/invoice.js";
 
 class App {
     constructor() {
         this.invoicePresentation = null;
-        this.InvoiceService = null;
-        this.dataAccess = null;
+        this.InvoiceBusiness = null;
+        this.InvoiceData = null;
     }
 
     init() {
-        this.dataAccess = new DataAccess();
-        this.InvoiceService = new InvoiceService(this.dataAccess);
-        this.invoicePresentation = new InvoicePresentation(this.InvoiceService);
+        this.InvoiceData = new InvoiceData();
+        this.InvoiceBusiness = new InvoiceBusiness(this.InvoiceData);
+        this.invoicePresentation = new InvoicePresentation(this.InvoiceBusiness);
         this.invoicePresentation.init();
     }
 }
