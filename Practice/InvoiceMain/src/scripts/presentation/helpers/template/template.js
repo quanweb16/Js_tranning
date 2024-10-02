638c5ad05c3bb053b1445cebc8e8379590e2f22a
minhquan2002 <minhquan663388@gmail.com>
2024-10-02 16:36:59 +0700

    Change directory structure

---
+++ Practice/InvoiceMain/src/scripts/presentation/helpers/template/template.js
@@ -0,0 +1,200 @@
+class Template {
+  static buildInvoices(data = []) {
+    return data.map((item) => this.buildInvoiceItem(item)).join("");
+  }
+
+  static buildInvoiceItem(data) {
+    return `
+          <li class="table-item" data-id="${data.id}">
+              <div class="invoice-item">
+                  <input type="checkbox" class="invoice-checkbox">
+                  <div class="invoice">${data.id}</div>
+                  <div class="profile-pic">
+                      <div class="name">${data.firstName || "No Name"} ${data.lastName || "No Name"}</div>
+                  </div>
+                  <div class="email">${data.email || "noemail@example.com"}</div>
+                  <div class="date"><i class="fas fa-calendar-alt"></i> ${data.date || ""}</div>
+                  <div class="status-1">${data.status || ""}</div>
+                  <button class="special"><i class="fas fa-star"></i></button>
+                  <div class="action">
+                      <div class="dots"></div>
+                      <div class="button-action">
+                          <div class="edit-btn">Edit</div>
+                          <div class="delete-btn">Delete</div>
+                      </div>
+                  </div>
+              </div>
+          </li>
+      `;
+  }
+
+  static buildInvoiceForm() {
+    return `
+          <div class="create-invoice-container">
+              <div class="popup-form">
+                      <div class="form-group">
+                          <label for="invoice-id">Invoice Id</label>
+                          <span id="invoice-id" class="invoice-id">#8181</span> 
+                          <span class="error-message-id"></span>
+                       </div>
+                      <div class="form-group">
+                          <label for="date">Date</label>
+                          <input type="date" id="date" name="date" required>
+                          <span class="error-message-date" id="date-error"></span>
+                      </div>
+                      <div class="form-group-inline">
+                          <div class="form-group">
+                              <label for="first-name">First Name</label>
+                              <input type="text" id="first-name" name="first-name" placeholder="First Name" required>
+                              <span class="error-message-first-name" id="first-name-error"></span>
+                          </div>
+                          <div class="form-group">
+                              <label for="last-name">Last Name</label>
+                              <input type="text" id="last-name" name="last-name" placeholder="Last Name" required>
+                              <span class="error-message-last-name" id="last-name-error"></span>
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <label for="email">Email</label>
+                          <input type="email" id="email" name="email" placeholder="name@gmail.com" required>
+                          <span class="error-message-email" id="email-error"></span>
+                      </div>
+                      <div class="form-group-inline">
+                          <div class="form-group">
+                              <label for="city">City</label>
+                              <input type="text" id="city" name="city" placeholder="City" required>
+                              <span class="error-message-city" id="city-error"></span>
+                          </div>
+                          <div class="form-group">
+                              <label for="region">Region</label>
+                              <input type="text" id="region" name="region" placeholder="Region" required>
+                              <span class="error-message-region" id="region-error"></span>
+                          </div>
+                      </div>
+                      <div class="form-group">
+                          <button type="submit" class="btn-invoice-create">Create Invoice</button>
+                      </div>
+                  </form>
+                  <button class="close-popup">
+                      <i class="fas fa-times"></i>
+                  </button>
+              </div>
+          </div>
+      `;
+  }
+
+  static buildInvoiceFormEdit() {
+    return `
+          <div class="edit-invoice-container">
+              <div class="popup-form-edit">
+                      <div class="form-group-edit">
+                           <label for="invoice-id">Invoice Id</label>
+                           <span id="invoice-id" class="invoice-id-edit">#8181</span> 
+                           <span class="error-message-id"></span>
+                      </div>
+                      <div class="form-group-edit">
+                          <label for="date">Date</label>
+                          <input type="date" id="date" name="date" required>
+                          <span class="error-message-date" id="date-error"></span>
+                      </div>
+                      <div class="form-group-inline">
+                          <div class="form-group-edit">
+                              <label for="first-name">First Name</label>
+                              <input type="text" id="first-name" name="first-name" placeholder="First Name" required>
+                              <span class="error-message-first-name" id="first-name-error"></span>
+                          </div>
+                          <div class="form-group-edit">
+                              <label for="last-name">Last Name</label>
+                              <input type="text" id="last-name" name="last-name" placeholder="Last Name" required>
+                              <span class="error-message-last-name" id="last-name-error"></span>
+                          </div>
+                      </div>
+                      <div class="form-group-edit">
+                          <label for="email">Email</label>
+                          <input type="email" id="email" name="email" placeholder="name@gmail.com" required>
+                          <span class="error-message-email" id="email-error"></span>
+                      </div>
+                      <div class="form-group-inline">
+                          <div class="form-group-edit">
+                              <label for="city">City</label>
+                              <input type="text" id="city" name="city" placeholder="City" required>
+                              <span class="error-message-city" id="city-error"></span>
+                          </div>
+                          <div class="form-group-edit">
+                              <label for="region">Region</label>
+                              <input type="text" id="region" name="region" placeholder="Region" required>
+                              <span class="error-message-region" id="region-error"></span>
+                          </div>
+                      </div>
+                      <div class="form-group-edit">
+                          <button type="submit" class="btn-invoice-edit">Edit Invoice</button>
+                      </div>
+                  </form>
+                  <button class="close-popup-edit">
+                      <i class="fas fa-times"></i>
+                  </button>
+              </div>
+          </div>
+      `;
+  }
+
+  static buildDeletePopup() {
+    return `
+          <div class="confirm-delete-container">
+              <h2>Are you sure you want to delete this invoice?</h2>
+              <div class="confirm-buttons">
+                  <button class="confirm-yes">Yes</button>
+                  <button class="confirm-no">No</button>
+              </div>
+          </div>
+      `;
+  }
+
+  static buildDeleteAllPopup() {
+    return `
+          <div class="confirm-deleteAll-container">
+              <h2>Are you sure you want to delete these invoices?</h2>
+              <div class="confirm-select-buttons">
+                  <button class="confirm-yes-all">Yes</button>
+                  <button class="confirm-no-all">No</button>
+              </div>
+          </div>
+      `;
+  }
+
+  static notificationPopup() {
+    return `
+          <div class="notificationDeleteAll-container">
+              <h2>Please select at least one invoice to delete!</h2>
+              <div class="confirm-buttons">
+                  <button class="confirm-yes">Yes</button>
+              </div>
+          </div>
+      `;
+  }
+
+  static notificationCreate() {
+    return `
+          <div class="notificationCreate-container">
+              <h2>Create successfully</h2>
+              <div class="confirm-buttons-success">
+                  <button class="confirm-ok-success">OK</button>
+              </div>
+          </div>
+      `;
+  }
+
+  static notificationEdit() {
+    return `
+          <div class="notificationEdit-container">
+              <h2>Edit successfully</h2>
+              <div class="confirm-buttons-edit-success">
+                  <button id="confirm-edit-success" class="confirm-edit-success">OK</button>
+              </div>
+          </div>
+      `;
+  }
+}
+
+export default Template;
+
---
+++ Practice/InvoiceMain/src/scripts/validation/invoice/validateInvoices.js
@@ -0,0 +1,109 @@
+import ERROR_MESSAGES from "../../constant/errorMessages";
+
+class ValidateInvoices {
+  validateInvoiceData(data, invoices) {
+    const errors = {};
+
+    if (!data.date) {
+      errors.date = ERROR_MESSAGES.DATE_REQUIRED;
+    } else if (!this.isValidDate(data.date)) {
+      errors.date = ERROR_MESSAGES.INVALID_DATE;
+    }
+
+    if (!data.email) {
+      errors.email = ERROR_MESSAGES.EMAIL_REQUIRED;
+    } else if (!this.isValidEmail(data.email)) {
+      errors.email = ERROR_MESSAGES.INVALID_EMAIL;
+    } else {
+      const existingInvoice = invoices.find(
+        (invoice) => invoice.id === data.id
+      );
+      if (existingInvoice) {
+        if (existingInvoice.email !== data.email) {
+          if (this.isDuplicateInvoice(invoices, data)) {
+            errors.email = ERROR_MESSAGES.EMAIL_EXISTS;
+          }
+        }
+      } else {
+        if (this.isDuplicateInvoice(invoices, data)) {
+          errors.email = ERROR_MESSAGES.EMAIL_EXISTS;
+        }
+      }
+    }
+
+    if (!data.firstName) {
+      errors.firstName = ERROR_MESSAGES.FIRST_NAME_REQUIRED;
+    } else if (!this.isValidFirstName(data.firstName)) {
+      errors.firstName = ERROR_MESSAGES.INVALID_FIRST_NAME;
+    }
+
+    if (!data.lastName) {
+      errors.lastName = ERROR_MESSAGES.LAST_NAME_REQUIRED;
+    } else if (!this.isValidLastName(data.lastName)) {
+      errors.lastName = ERROR_MESSAGES.INVALID_LAST_NAME;
+    }
+
+    if (!data.city) {
+      errors.city = ERROR_MESSAGES.CITY_REQUIRED;
+    } else if (!this.isValidCity(data.city)) {
+      errors.city = ERROR_MESSAGES.INVALID_CITY;
+    }
+
+    if (!data.region) {
+      errors.region = ERROR_MESSAGES.REGION_REQUIRED;
+    } else if (!this.isValidRegion(data.region)) {
+      errors.region = ERROR_MESSAGES.INVALID_REGION;
+    }
+
+    if (Object.keys(errors).length > 0) {
+      return { success: false, errors };
+    }
+
+    return { success: true };
+  }
+
+  isValidDate(date) {
+    const inputDate = new Date(date);
+    const minDate = new Date("2020-01-01");
+    const currentDate = new Date();
+    if (inputDate < minDate) {
+      return false;
+    }
+    if (inputDate > currentDate) {
+      return false;
+    }
+    return true;
+  }
+
+  isValidFirstName(firstName) {
+    const isOnlyLettersAndSpaces = /^[\p{L}\s]+$/u.test(firstName);
+    return firstName && firstName.length >= 2 && isOnlyLettersAndSpaces;
+  }
+
+  isValidLastName(lastName) {
+    const isOnlyLettersAndSpaces = /^[\p{L}\s]+$/u.test(lastName);
+    return lastName && lastName.length >= 2 && isOnlyLettersAndSpaces;
+  }
+
+  isValidEmail(email) {
+    const re = /^[a-zA-Z][\w.-]*@[a-zA-Z][\w.-]+\.[a-zA-Z]{2,}$/;
+    return re.test(email);
+  }
+
+  isValidCity(city) {
+    const re = /^[\p{L}\s]+$/u;
+    return re.test(city);
+  }
+
+  isValidRegion(region) {
+    const re = /^[\p{L}\s]+$/u;
+    return re.test(region);
+  }
+
+  isDuplicateInvoice(invoices, data) {
+    return invoices.some(
+      (invoice) => invoice.email === data.email && invoice.id !== data.id
+    );
+  }
+}
+export default ValidateInvoices;
